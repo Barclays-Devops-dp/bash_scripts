b9fa9033b8ec08d0ab9592f4b30f87293374f3bc
EC2 Default User <ec2-user@ip-172-31-3-106.eu-west-2.compute.internal>
2024-05-27 20:02:26 +0000

    my bash script files

---
+++ my_commands.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+my_name="Josephine"
+num_of_script_yesterday=20
+num_of_script_today=10
+echo "Hey, i am actually creating folders and making magic happen ..... hahaha!, ${my_name}  on the mic"
+echo "Yesterday i ran ${num_of_script_yesterday}"
+echo "Today i am running ${num_of_script_today}"
+echo "In total i between yday and today i have run $((num_of_script_yesterday + num_of_script_today))"
+
+echo "for records sake, i am marking the date for today. And today's date is $(date)"
+mkdir shell_scripting
+touch i_am_learning_shell_script
+ls
+mv i_am_learning_shell_script shell_scripting/
+echo "still on the move engineer ${my_name}"
+ls
+tree
+sudo yum install tree
+sudo yum install tree -y
+sudo yum install git
+sudo yum install git  -y
+history
+
+echo "this is yours truly Engineer ${my_name}"
+
