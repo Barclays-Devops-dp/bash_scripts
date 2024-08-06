047fecb724818458fe2a86b8296d468e23a61f3a
devops2024dp <devops2024@datapandas.com>
2024-08-06 22:07:04 +0100

    Update for_loops.sh

--- for_loops.sh
+++ for_loops.sh
@@ -14,3 +14,86 @@ array_of_users=("david" "ec2-user")
 for file in /home/ec2-user/* ; do
   echo $file
 done
+
+
+regions_to_provision_to=("us-east-1" "ca-east-2" "eu-west-2")
+
+echo ${regions_to_provision_to[@]}
+
+# for loop iterates ( goes through the items one after the other)
+# 01. structure of the "for loop"
+# iterable: anything that we can iterate over.
+# for temporary_variable_to_the_item_pick in ITERABLE; do 
+#     do something with the item you picketed from the array/list
+# done
+
+for item in ${regions_to_provision_to[@]}; do 
+    echo "this is the item i have picked CURRENTLY: ${item}"
+done
+
+#get items from a paths also as an iterable. 
+for something in /home/ec2-user/bash_scripting/* ; do 
+    echo "this item is also found ${something}"
+done
+
+# printing sequence of numbers and stepping in 2s
+echo "  "
+echo "  "
+echo "======================="
+for num in {2..20..2}; do
+    echo "Number is: $num"
+done
+
+echo "  "
+echo "  "
+echo "======================="
+# combine for loop with if clause
+for num in {1..100..10}
+do
+   if [[ $num -gt 50  ]]; then 
+       echo "number is greater than 50"
+   fi 
+
+echo "num is: $num"
+done
+
+
+echo "  "
+echo "  "
+echo "======================="
+# using "continue" keyword to stop iteration. 
+for region in eu-west-1 us-east-2 mx-west-1 za-east-1
+do
+   if [[ $region  == "mx-west-1"  ]]; then 
+       echo "skipping MEXIC0"
+       continue
+   fi 
+
+echo "We are provisiong 4 ec2 instances in region: $region"
+done
+
+echo "  "
+echo "  "
+echo "=*********************************************===="
+declare -A country
+country["gh"]=ghana
+country["uk"]="united kingdom"
+country["us"]=USA
+
+for country in ${country[@]}
+do
+
+   # this is a "second for loop" inside a "first for loop"
+    for town in "accra" "london" "new-york"
+    do
+        # using "if and eleif"
+       if [[ $town == "accra" ]]; then 
+          echo "${town} is the capital of ${country}"
+        elif [[ $town == "london" ]]; then 
+           echo "${town} is the capital of ${country}"
+        elif [[ $town == "new-york" ]]; then 
+           echo "${town} is the capital of ${country}"
+       fi
+    done
+
+done
